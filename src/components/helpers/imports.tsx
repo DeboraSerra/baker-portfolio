c44ced345b6ebc006a8729480558324f32007c15
Débora Serra <debora.r.serra@gmail.com>
2024-09-19 17:23:05 -0700

    feat: add pictures

---
+++ src/components/helpers/imports.tsx
@@ -0,0 +1,23 @@
+import abacaxiCoco from "../../../assets/images/cakes/abacaxi-coco.jpg";
+import abacaxiCoco3 from "../../../assets/images/cakes/abacaxi-com-coco&choc-morango.jpg";
+import abacaxiCoco4 from "../../../assets/images/cakes/abacaxi-com-coco&choc-morango2.jpg";
+import abacaxiCoco2 from "../../../assets/images/cakes/abacaxi-com-coco.jpg";
+import amendoaChocolate from "../../../assets/images/cakes/amendoa-chocolate.jpg";
+import astromelia from "../../../assets/images/cakes/astromelia-2-andares.jpg";
+import brigadeiro from "../../../assets/images/cakes/brigadeiro.jpg";
+import cappuccino2 from "../../../assets/images/cakes/cappuccino-fatia.jpg";
+import cappuccino from "../../../assets/images/cakes/cappuccino.png";
+import carved from "../../../assets/images/cakes/carved.jpg";
+
+export {
+  abacaxiCoco,
+  abacaxiCoco2,
+  abacaxiCoco3,
+  abacaxiCoco4,
+  amendoaChocolate,
+  astromelia,
+  brigadeiro,
+  cappuccino,
+  cappuccino2,
+  carved,
+};
